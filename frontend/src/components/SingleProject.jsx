e004cd90ad2cd8618eec614793e30aee18aa2604
Hamza <hamzaaziz086@gmail.com>
2024-07-17 20:03:58 +0500

    completed projects section

--- frontend/src/components/SingleProject.jsx
+++ frontend/src/components/SingleProject.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const SingleProject = () => {
+const SingleProject = ({ title, description, githubLink, className }) => {
   return (
-    <div className='w-1/4'>
-        <h1>Title</h1>
-        <p>Description</p>
+    <div className={className}>
+      <div className='space-y-5'>
+        <h1 className='text-2xl font-bold'>{title}</h1>
+        <p className='text-justify'>{description}</p>
+      </div>
+
+      <button className='mt-5 bg-gray-200 text-gray-900 w-full px-6 py-2 rounded-3xl'>
+        <a href={githubLink} target='_blank' className='flex items-center justify-center space-x-2'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-code"><polyline points="16 18 22 12 16 6" /><polyline points="8 6 2 12 8 18" /></svg>
+          <span>Source Code</span>
+        </a>
+      </button>
     </div>
   )
 }
--- frontend/src/sections/Projects.jsx
+++ frontend/src/sections/Projects.jsx
@@ -3,16 +3,31 @@ import SingleProject from '../components/SingleProject'
 
 const Projects = () => {
   return (
-    <div className='mt-16'>
-        <h1 className='text-4xl flex justify-center items-center space-x-3'>
+    <div className='py-20 border-b border-gray-700 rounded-ee-[50px] rounded-es-[50px] sm:rounded-ee-[125px] sm:rounded-es-[125px]'>
+        <h1 className='text-4xl font-bold flex justify-center items-center space-x-3'>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-folder-kanban h-10 w-10"><path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z"/><path d="M8 10v4"/><path d="M12 10v2"/><path d="M16 10v6"/></svg>
             <span>Key Projects</span>
         </h1>
 
-        <div className='grid grid-cols-3 mt-16'>
-            <SingleProject />
-            <SingleProject />
-            <SingleProject />
+        <div className='grid grid-cols-3 gap-10 mt-16 w-3/4 mx-auto'>
+            <SingleProject
+              title={'Blogs App'}
+              description={'This web application provides a platform for users to create, read, update and delete blog posts. Each blog post has a title, summary, image and blog content.For this users have to first go through authentication process which is acheived using JSON Web Tokens or simply JWT. Moreover I also added a rich text editor React Quill so that users can add content in their desired format.'}
+              githubLink={'https://github.com/ItsHamzaAziz/BlogsAppMERN'}
+              className='border-2 border-red-600 rounded-3xl hover:shadow-lg hover:shadow-red-600 p-8 cursor-pointer flex flex-col justify-between'
+            />
+            <SingleProject
+              title={'Events App'}
+              description={'In this events app, I utilized React and Tailwind CSS for the frontend, and Django along with the Django REST Framework for the backend. The application allows authenticated users to add, update and delete events, while all users can view event listings and details. Additionally, the app features event search functionality and filtering by category for enhanced user experience.'}
+              githubLink={'https://github.com/ItsHamzaAziz/EventsAppDjangoReact'}
+              className='border-2 border-green-600 rounded-3xl hover:shadow-lg hover:shadow-green-600 p-8 space-y-5 cursor-pointer flex flex-col justify-between'
+            />
+            <SingleProject
+              title={'Recipe Sharing App'}
+              description={'This web application enables users to share and save recipes, similar to saving posts on social media platforms. It features user authentication, CRUD operations, and image uploads. Users can also apply filters to find specific recipes. For enhanced security, UUIDs were used instead of regular IDs. The project was built using Django and styled with Tailwind CSS.'}
+              githubLink={'https://github.com/ItsHamzaAziz/FlavorQuest'}
+              className='border-2 border-blue-600 rounded-3xl hover:shadow-lg hover:shadow-blue-600 p-8 space-y-5 cursor-pointer flex flex-col justify-between'
+            />
         </div>
     </div>
   )
