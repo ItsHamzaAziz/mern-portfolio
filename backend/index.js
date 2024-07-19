6da73036da311e464d196dde73f6d4c761956816
Hamza <hamzaaziz086@gmail.com>
2024-07-19 14:29:59 +0500

    checking vercel cors issue

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type']
-// }))
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
+// app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI)
 
